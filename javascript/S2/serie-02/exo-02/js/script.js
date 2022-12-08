c9930b38a83cebc030e1d1007c909c013a0e965e
Brendon Desvaux <78964703+BrendonDesvaux@users.noreply.github.com>
2022-12-08 16:02:54 +0100

    Update correction of ex2

--- javascript/S2/serie-02/exo-02/js/script.js
+++ javascript/S2/serie-02/exo-02/js/script.js
@@ -3,7 +3,7 @@ let secondCmd = prompt('Entrez un second nombre');
 
 //calculate firstCMD power secondCMD with a loop
 let result = 1;
-for (let i = 0; i <= secondCmd; i++) {
+for (let i = 0; i < secondCmd; i++) {
     result *= firstCmd;
 }
 console.log(firstCmd + " puissance " + secondCmd + " egal " + result);
--- javascript/S2/serie-02/exo-05/js/script.js
+++ javascript/S2/serie-02/exo-05/js/script.js
@@ -9,3 +9,32 @@ while (i <= 10) {
 }
 
 console.log(result);
+
+//create factorial of number using while loop
+let factorial = 1;
+let j = 1;
+while (j <= number) {
+    factorial *= j;
+    j++;
+}
+console.log(number + "! = " + factorial);
+
+//Create power of number using while loop
+let power = 1;
+let k = 1;
+while (k <= number) {
+    power *= number;
+    k++;
+}
+console.log(number + " puissance " + number + " = " + power);
+
+//write in console if it's even or odd using while loop
+let l = 0;
+while (l < 10) {
+    if (l % 2 === 0) {
+        console.log(l + " est pair");
+    } else {
+        console.log(l + " est impair");
+    }
+    l++;
+}
