4443b8080cb0a28db8b55b7e60a76181b69b9e5e
Brendon Desvaux <78964703+BrendonDesvaux@users.noreply.github.com>
2023-01-02 13:22:21 +0100

    update date ex

--- javascript/S6/serie-02/ex2/js/script.js
+++ javascript/S6/serie-02/ex2/js/script.js
@@ -1,5 +1,22 @@
 //show the age of mozard when he died
-const mozard = new Date(1756, 1, 27)
-const mozardDeath = new Date(1791, 11, 5)
-const mozardAge = mozardDeath.getFullYear() - mozard.getFullYear()
-console.log(mozardAge)
+const mozard = new Date(1756, 1, 27);
+const today = new Date();
+const mozardDeath = new Date(1791, 11, 5);
+const mozardAge = mozardDeath.getFullYear() - mozard.getFullYear();
+let mozardDeathMonth = mozardDeath.getMonth() - mozard.getMonth();
+let mozardDeathDay = mozardDeath.getDate() - mozard.getDate();
+let mozardMonth = today.getMonth() - mozard.getMonth();
+let mozardDay = today.getDate() - mozard.getDate();
+let MozardActualAge = today.getFullYear() - mozard.getFullYear();
+if (mozardMonth < 0 || (mozardMonth === 0 && today.getDate() < mozard.getDate())) {
+    MozardActualAge--;
+}
+if ((mozardMonth < 0) || (mozardDeathMonth < 0)) {
+    mozardMonth = 12 + mozardMonth;
+}
+if ((mozardDay < 0) || (mozardDeathDay <= 0)) {
+    mozardDay = 31 + mozardDay;
+    mozardDeathDay = 31 + mozardDeathDay;
+}
+console.log('Mozart est mort a ' + mozardAge + ' ans, ' + mozardDeathMonth + ' mois et ' + mozardDeathDay + ' jours');
+console.log('Mozart aurait eu ' + MozardActualAge + ' ans, ' + mozardMonth + ' mois et ' + mozardDay + ' jours' + ' aujourd\'hui');
--- javascript/S6/serie-02/ex3/js/script.js
+++ javascript/S6/serie-02/ex3/js/script.js
@@ -1,3 +1,12 @@
+const ageAnnée = document.querySelector('#age');
+const ageMois = document.querySelector('#ageC');
+const button = document.querySelector('button');
+button.addEventListener('click', function(e) {
+    e.preventDefault();
+    ageAnnée.innerHTML = calculateAge();
+    ageMois.innerHTML = calculateAgePrecisely();
+});
+
 //create function to calculate age
 function calculateAge(birthYear) {
     let bd = document.querySelector("#birthday");
@@ -15,19 +24,15 @@ function calculateAgePrecisely(birthYear, birthMonth, birthDay) {
     var today = new Date();
     var age = today.getFullYear() - year;
     var month = today.getMonth() - bd.value.substring(5, 7);
+    var day = today.getDate() - bd.value.substring(8, 10);
     if (month < 0 || (month === 0 && today.getDate() < bd.value.substring(8, 10))) {
         age--;
     }
-    return age + 'ans et ' + month + "mois";
+    if (month < 0) {
+        month = 12 + month;
+    }
+    if (day < 0) {
+        day = 31 + day;
+    }
+    return age + " ans et " + month + " mois " + day + " jours";
 }
-
-const ageAnnée = document.querySelector('#age');
-const ageMois = document.querySelector('#ageC');
-const button = document.querySelector('button');
-button.addEventListener('click', function(e) {
-    e.preventDefault();
-    console.log(calculateAge());
-    console.log(calculateAgePrecisely());
-    ageAnnée.innerHTML = calculateAge();
-    ageMois.innerHTML = calculateAgePrecisely();
-});
